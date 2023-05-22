fe9eeeb3bba0f797f7248fb185a1c26b78c7950f
Anson Cheng <Anson.12666@icloud.com>
2023-05-22 09:59:57 +0800

    feat: login screen (not complete)

---
+++ qwik-app/src/routes/controlPanel/layout.tsx
@@ -0,0 +1,11 @@
+import { component$, Slot } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <div class="main">
+      <div>AAAAA</div>
+      <Slot />
+        
+    </div>
+  );
+});
--- qwik-app/src/routes/index.tsx
+++ qwik-app/src/routes/index.tsx
@@ -1,26 +1,33 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
 import { useAuthSession } from "~/routes/plugin@auth";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { useNavigate, type DocumentHead } from "@builder.io/qwik-city";
 import RegisterSelector from "~/components/login/loginForm";
-import ControlPanel from "~/routes/controlPanel/controlPanel";
 
 // https://colorhunt.co/palette/f5f0bbdbdfaab3c89073a9ad
 export default component$(() => {
   const session = useAuthSession();
+  // const nav = useNavigate();
+
+  // useVisibleTask$(async ({ track }) => {
+  //   track(() => session.value);
+  //   const value = session.value;
+  //   console.log("session.value", value);
+  //   if (value) {
+  //     await nav("/controlPanel");
+  //   }
+  // });
 
   return (
-    <div class="userAuth">
-      {session.value ? <ControlPanel /> : <RegisterSelector />}
-    </div>
+    <div class="userAuth">{session.value ? <></> : <RegisterSelector />}</div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "OCamp CampBook",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "This is a e-version of the OCamp CampBook",
     },
   ],
 };
